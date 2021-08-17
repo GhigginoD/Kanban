7b416a720a9dc12266bd5b0b08f226d28dda24ca
davi <davi.ghiggino@concilie.com.br>
2021-08-16 22:38:12 -0300

    Contrução do js utilizando draggable e droppable

---
+++ js/drag_drop.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#lista_to_do li,#lista_in_progress li').draggable({
+        helper: 'clone', 
+        revert: 'invalid',
+        
+    });
+
+    $('#to_do').droppable({
+        drop: function(event, ui) {
+            $('#lista_to_do').append(ui.draggable);
+        }
+    });
+
+    $('#in_progress').droppable({
+        drop: function(event, ui) {
+            $('#lista_in_progress').append(ui.draggable);
+        }
+    });
+
+    $('#done').droppable({
+        drop: function(event, ui) {
+            $('#lista_done').append(ui.draggable);
+        }
+    });
+
+    // start: function(event, ui) {console.log(event);},
+    // over: function( event, ui ) {console.log(event)},
+    // stop: function(event, ui) {console.log(event);}
+});
